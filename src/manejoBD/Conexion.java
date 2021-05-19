4bcb682dfe28f07fe610a2542f561949eb19ea63
Carlos <77995794+EightSnakes@users.noreply.github.com>
2021-05-18 19:32:36 -0500

    Add files via upload

---
+++ src/manejoBD/Conexion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manejoBD;
+import java.sql.*;
+/**
+ *
+ * @author Snake
+ */
+public class Conexion {
+    Statement stmt = null;
+    Connection conect = null;
+    public static Connection sql;
+    public static Connection conectar(){
+        try{
+            String connectionUrl = "jdbc:sqlserver://localhost:1433;database=BDSI;user=sa; password = 123";
+            sql = DriverManager.getConnection(connectionUrl);
+            System.out.print("Conectado.");
+        }
+        catch(SQLException ex){
+            System.out.println("Error " + ex);
+        }
+        return sql;
+    }
+}
